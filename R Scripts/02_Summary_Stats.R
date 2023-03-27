133266222fb83e8da4e6d706578d44b36c85d097
Yi Ning Wong <yiningw@uchicago.edu>
2023-03-27 11:20:41 -0400

    [summary-stats] upload basic code

--- R Scripts/02_Summary_Stats.R
+++ R Scripts/02_Summary_Stats.R
@@ -1,15 +1,33 @@
-
 ##Education for Employment
+## Last modified by YNW on 3/27/23
+
+## Set up 
+rm(list=ls())
 
 library(tidyverse)
 
+# Set wd
+parent_dir <- dirname(getwd())
 
-## Load Data from USB
-user <- Sys.getenv("USERNAME")
-df <- read.csv(paste0("C:/Users/", user,"/Documents/GitHub/EducationForEmployment/ContactsProcessed.csv"))
-
+## Read file
+df <- read.csv(paste0(parent_dir,"/ContactsProcessed.csv"))
 
 # groups to summarize 
-groups <- colnames(df)
-colnames(df)
-# by country by months of retention everytime
+groups <- c("Gender", "Nationality", "Speaks.Arabic", 
+            "Speaks.English", "Age", "Beneficiary.Specialization",
+            "Level.of.Education")
+
+# Create list of the retention groups so we can loop over outcomes by each of the group
+retention_groups <- c("pl_grad", "pl_3", "pl_6", "pl_9", "pl_12")
+
+# create mean by country by months of retention every time
+calculate_mean <- function(df) {
+  for (group in retention_groups) {
+  df %>%
+    group_by(Country.of.Programming, group) %>%
+    summarize_at(vars(cols), mean, na.rm = TRUE)
+  
+  }
+}
+
+calculate_mean(df)
